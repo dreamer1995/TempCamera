95c693eba89914cad558b733be60770272785146
dreamer1995 <dreamer199506@126.com>
2021-03-23 00:09:25 +0800

    update random

--- hw3d/VolumeCal.hlsl
+++ hw3d/VolumeCal.hlsl
@@ -42,17 +42,26 @@ float4 main(float2 uv : Texcoord) : SV_Target
 
 	float3 accumFog = 0.0f;
 
+	ditherPattern[4][4] = {{0.0f, 0.5f, 0.125f, 0.625f},
+						   {0.75f, 0.22f, 0.875f, 0.375f},
+						   {0.1875f, 0.6875f, 0.0625f, 0.5625},
+						   {0.9375f, 0.4375f, 0.8125f, 0.3125}};
+	
 	for (int i = 0; i < NB_STEPS; i++)
 	{
 		float4 shadowHomoPos = ToShadowHomoSpace(float4(currentPosition, 1.0f), shadowMatrix_VP);
 		const float3 spos = shadowHomoPos.xyz / shadowHomoPos.w;
-		float shadowMapValue = Shadow(shadowHomoPos, smap);
+		float shadowMapValue = smap.SampleLevel(splr, spos.xy, 0).r;
 
 		if (shadowMapValue >= spos.z)
 		{
 			accumFog += ComputeScattering(dot(rayDirection, direction)) * DdiffuseColor * DdiffuseIntensity;
 
 		}
+				
+		// Offset the start position.
+		startPosition += step * ditherValue[i % 16];
+		
 		currentPosition += step;
 	}
 	accumFog /= NB_STEPS;
