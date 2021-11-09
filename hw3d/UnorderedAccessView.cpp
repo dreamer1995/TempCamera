772d78d1655c6476eb306689966e0514843ca8c3
Fallshill <dreamer199506@126.com>
2021-11-09 23:33:41 +0800

    UVA Class half done

--- hw3d/UnorderedAccessView.cpp
+++ hw3d/UnorderedAccessView.cpp
@@ -5,8 +5,9 @@
 namespace Bind
 {
 	namespace wrl = Microsoft::WRL;
-	
-	UnorderedAccessView::UnorderedAccessView(Graphics& gfx, UINT slot, UINT shaderIndex)
+
+	template <typename T>
+	UnorderedAccessView<T>::UnorderedAccessView(Graphics& gfx, T& dataType, UINT width, UINT height, UINT slot, UINT shaderIndex)
 		:
 		slot(slot),
 		shaderIndex(shaderIndex)
@@ -14,33 +15,75 @@ namespace Bind
 		INFOMAN(gfx);
 
 		// The compute shader will need to output to some buffer so here we create a GPU buffer for that.
-		D3D11_BUFFER_DESC cbd;
-		cbd.BindFlags = D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
-		cbd.ByteWidth = sizeof(consts);
-		cbd.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
-		cbd.StructureByteStride = 4;	// We assume the output data is in the RGBA format, 8 bits per channel
+		D3D11_BUFFER_DESC uabd;
+		uabd.BindFlags = D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
+
+		DataPerCell dataCell;
+
+		uabd.ByteWidth = sizeof(dataCell) * width * height;
+		uabd.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+		uabd.StructureByteStride = 4;	// We assume the output data is in the RGBA format, 8 bits per channel
 
 		ID3D11Buffer* UAGPUBuffer;
-		D3D11_SUBRESOURCE_DATA csd = {};
-		csd.pSysMem = &consts;
-		GFX_THROW_INFO(GetDevice(gfx)->CreateBuffer(&cbd, &csd, &UAGPUBuffer));
+		GFX_THROW_INFO(GetDevice(gfx)->CreateBuffer(&uabd, NULL, &UAGPUBuffer));
 
-		// The view we need for the output is an unordered access view. This is to allow the compute shader to write anywhere in the buffer.
-		D3D11_BUFFER_DESC descBuf;
-		ZeroMemory(&descBuf, sizeof(descBuf));
-		m_destDataGPUBuffer->GetDesc(&descBuf);
+		D3D11_UNORDERED_ACCESS_VIEW_DESC uavd;
+		uavd.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+		uavd.Buffer.FirstElement = 0;
 
-		D3D11_UNORDERED_ACCESS_VIEW_DESC descView;
-		ZeroMemory(&descView, sizeof(descView));
-		descView.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
-		descView.Buffer.FirstElement = 0;
+		uavd.Format = DXGI_FORMAT_UNKNOWN;      // Format must be must be DXGI_FORMAT_UNKNOWN, when creating a View of a Structured Buffer
+		uavd.Buffer.NumElements = uabd.ByteWidth / uabd.StructureByteStride;
 
-		descView.Format = DXGI_FORMAT_UNKNOWN;      // Format must be must be DXGI_FORMAT_UNKNOWN, when creating a View of a Structured Buffer
-		descView.Buffer.NumElements = descBuf.ByteWidth / descBuf.StructureByteStride;
+		GFX_THROW_INFO(GetDevice(gfx)->CreateUnorderedAccessView(UAGPUBuffer, &uavd, &pUnorderedAccessView));
+	}
 
-		if (FAILED(m_pd3dDevice->CreateUnorderedAccessView(m_destDataGPUBuffer, &descView, &m_destDataGPUBufferView)))
-			return false;
+	template <typename T>
+	void UnorderedAccessView<T>::Bind(Graphics& gfx) noxnd
+	{
+		assert(shaderIndex & 0b00111111);
+		INFOMAN_NOHR(gfx);
+		if (shaderIndex & 0b00010000)
+		{
+			GFX_THROW_INFO_ONLY(GetContext(gfx)->VSSetShaderResources(slot, 1, pTextureView.GetAddressOf()));
+		}
+		if (shaderIndex & 0b00001000)
+		{
+			GFX_THROW_INFO_ONLY(GetContext(gfx)->HSSetShaderResources(slot, 1, pTextureView.GetAddressOf()));
+		}
+		if (shaderIndex & 0b00000100)
+		{
+			GFX_THROW_INFO_ONLY(GetContext(gfx)->DSSetShaderResources(slot, 1, pTextureView.GetAddressOf()));
+		}
+		if (shaderIndex & 0b00000010)
+		{
+			GFX_THROW_INFO_ONLY(GetContext(gfx)->GSSetShaderResources(slot, 1, pTextureView.GetAddressOf()));
+		}
+		if (shaderIndex & 0b00000001)
+		{
+			GFX_THROW_INFO_ONLY(GetContext(gfx)->PSSetShaderResources(slot, 1, pTextureView.GetAddressOf()));
+		}
+		if (shaderIndex & 0b00100000)
+		{
+			GFX_THROW_INFO_ONLY(GetContext(gfx)->CSSetShaderResources(slot, 1, pTextureView.GetAddressOf()));
+		}
+	}
 
-		return true;
+	template<typename T>
+	std::shared_ptr<UnorderedAccessView<T>> UnorderedAccessView<T>::Resolve(Graphics& gfx, T& dataType, UINT width, UINT height, UINT slot, UINT shaderIndex)
+	{
+		return Codex::Resolve<UnorderedAccessView>(gfx, dataType, width, height, slot, shaderIndex);
+	}
+
+	template<typename T>
+	std::string UnorderedAccessView<T>::GenerateUID(UINT slot, UINT shaderIndex)
+	{
+		using namespace std::string_literals;
+		return typeid(UnorderedAccessView).name() + "#"s + "#" + std::to_string(slot) + "#" + std::to_string(shaderIndex);
+	}
+
+	template<typename T>
+	std::string UnorderedAccessView<T>::GetUID() const noexcept
+	{
+		return GenerateUID(slot, shaderIndex);
 	}
 }
--- hw3d/UnorderedAccessView.h
+++ hw3d/UnorderedAccessView.h
@@ -2,12 +2,21 @@
 #include "Bindable.h"
 namespace Bind
 {
+	template <typename T>
 	class UnorderedAccessView : public Bindable
 	{
 	public:
-		UnorderedAccessView(Graphics& gfx, UINT slot, UINT shaderIndex);
+		struct DataPerCell
+		{
+			T r;
+			T g;
+			T b;
+			T a;
+		};
+
+		UnorderedAccessView(Graphics& gfx, T& dataType, UINT width, UINT height, UINT slot, UINT shaderIndex);
 		void Bind(Graphics& gfx) noxnd override;
-		static std::shared_ptr<UnorderedAccessView> Resolve(Graphics& gfx, UINT slot = 0, UINT shaderIndex = 0b1u);
+		static std::shared_ptr<UnorderedAccessView> Resolve(Graphics& gfx, T& dataType, UINT width, UINT height, UINT slot = 0, UINT shaderIndex = 0b1u);
 		static std::string GenerateUID(UINT slot, UINT shaderIndex);
 		std::string GetUID() const noexcept override;
 	private:
